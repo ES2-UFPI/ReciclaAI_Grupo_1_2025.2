4b53f1ac6c222a5526d4ba7448a74859db22f560
theguestmanuel <emmanuel.guimaraes@ufpi.edu.br>
2025-10-22 19:14:41 -0300

    "Adicão definitiva"

--- backend/src/main/java/br/ufpi/recicle_ai/domain/model/dto/ProdutorDTO.java
+++ backend/src/main/java/br/ufpi/recicle_ai/domain/model/dto/ProdutorDTO.java
@@ -1,13 +1,15 @@
 package br.ufpi.recicle_ai.domain.model.dto;
 
+import java.util.List;
+
 import br.ufpi.recicle_ai.domain.dto.AgenteDTO;
+import br.ufpi.recicle_ai.domain.model.Itens;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class ProdutorDTO extends AgenteDTO {
-    
-    
-    
+    private List<Itens> itens;
+
 }
